13480e91a58549bbdce18bda95f4e26845fca459
SirVincelot24 <132359317+SirVincelot24@users.noreply.github.com>
2025-12-04 18:20:09 +0100

    both halves solved! only the german #Pfusch can describe my solution for the 2nd half properly

--- day4/problem2.py
+++ day4/problem2.py
@@ -16,32 +16,40 @@ def getAdjacent8Places(input : list, space : tuple) -> list:
     output.append(input[rowAbove][elementBefore:elementAfter])
     if rowAbove == y:
         output[0] = []
-    # print(output[0])
     output.append(input[y][elementBefore:elementAfter])
-    # print(output[1])
     output.append(input[rowBelow][elementBefore:elementAfter])
     if rowBelow == y:
         output[2] = []
-    # print(output[2])
     return output
 
 def countNestedList(listToCount : list, characterToCount) -> int:
     return sum([listToCount[i].count(characterToCount) for i in range(len(listToCount))])
 
+def prg():
+    global accessiblePaperRolls
+    for y, line in enumerate(parsedInput):
+        for x, elem in enumerate(line):
+            if elem != "@":
+                continue
+            adjacent8Places = getAdjacent8Places(parsedInput, (x, y))
+            if countNestedList(adjacent8Places, "@") < 5:
+                # print(adjacent8Places[0])
+                # print(adjacent8Places[1])
+                # print(adjacent8Places[2])
+                # print()
+                accessiblePaperRolls += 1
+                parsedInput[y][x] = "x"
+
 # Program
+
+global accessiblePaperRolls
+accessiblePaperRolls = 0
+
+for _ in range(200):
+    prg()
+
 for i in parsedInput:
     [print(j, end="") for j in i]
     print()
-accessiblePaperRolls = 0
-for y, line in enumerate(parsedInput):
-    for x, elem in enumerate(line):
-        if elem != "@":
-            continue
-        adjacent8Places = getAdjacent8Places(parsedInput, (x, y))
-        if countNestedList(adjacent8Places, "@") < 5:
-            # print(adjacent8Places[0])
-            # print(adjacent8Places[1])
-            # print(adjacent8Places[2])
-            # print()
-            accessiblePaperRolls += 1
+
 print(accessiblePaperRolls)
