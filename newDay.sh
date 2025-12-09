3d428e3c0de1d5f6d8b151f47934fe703ab8d017
vincent <vin.beck24@gmail.com>
2025-12-09 22:53:57 +0100

    improved newDay.sh script

--- newDay.sh
+++ newDay.sh
@@ -1,4 +1,22 @@
-mkdir day"$1" && cd day"$1"
-touch input.txt
-touch testinput.txt
-cp ../template_problem1.py ./problem1.py
+if [ $# -eq 1 ]; then
+  mkdir day"$1"
+  cd day"$1" || exit
+  touch input.txt
+  touch testinput.txt
+  cp ../template_problem1.py ./problem1.py
+  cd ..
+fi
+if [ $# -eq 2 ]; then
+  for (( i = $1; i <= $2; i++ )); do
+    mkdir day"$i"
+    cd day"$i" || exit
+    touch input.txt
+    touch testinput.txt
+    cp ../template_problem1.py ./problem1.py
+    cd ..
+  done
+fi
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <day> [<tillDay>]"
+  exit 1
+fi
