067482663d8c34574f05c965af1fb871ae1543ac
vincent <vin.beck24@gmail.com>
2025-12-21 00:05:45 +0100

    created day12 folder structure

---
+++ day12/problem1.py
@@ -0,0 +1,10 @@
+# Parser
+parsedInput = []
+with open("input.txt", "r") as puzzleInput:
+    for line in puzzleInput.readlines():
+        parsedInput.append(line.strip())
+
+# Functions
+
+# Program
+pass
